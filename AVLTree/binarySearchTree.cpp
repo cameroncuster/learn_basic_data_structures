a2d87350e1ff85af7fc5b164885bcfc9ce06dadf
Cameron Custer <Cameron.Custer@mines.sdsmt.edu>
2020-12-07 16:31:07 -0700

    overloaded equal operator written and tested

--- AVLTree/binarySearchTree.cpp
+++ AVLTree/binarySearchTree.cpp
@@ -238,3 +238,43 @@ TEST_CASE( "COPY CONSTRUCTOR" )
         REQUIRE( sout.str( ) == soutcpy.str( ) );
     }
 }
+
+TEST_CASE( "OVERLOADED EQUAL" )
+{
+    binarySearchTree bst;
+
+    stringstream sout;
+    stringstream soutcpy;
+
+    SECTION( "EMPTY" )
+    {
+        binarySearchTree bstcpy = bst;
+        bst.print( sout );
+        bstcpy.print( soutcpy );
+        REQUIRE( sout.str( ) == soutcpy.str( ) );
+    }
+
+    bst.insert( 0 );
+
+    SECTION( "SINGLE VALUE" )
+    {
+        binarySearchTree bstcpy = bst;
+        bst.print( sout );
+        bstcpy.print( soutcpy );
+        REQUIRE( sout.str( ) == soutcpy.str( ) );
+    }
+
+    bst.insert( 3 );
+    bst.insert( 2 );
+    bst.insert( 1 );
+    bst.insert( 4 );
+    bst.insert( 5 );
+
+    SECTION( "MULTIPLE VALUES" )
+    {
+        binarySearchTree bstcpy = bst;
+        bst.print( sout );
+        bstcpy.print( soutcpy );
+        REQUIRE( sout.str( ) == soutcpy.str( ) );
+    }
+}
